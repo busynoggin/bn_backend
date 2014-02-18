84f883de81f2bddb65c59db6db7add6e908a95b2
Jeff Segars <jeff@busynoggin.com>
2014-02-18 14:07:05 -0600

    Set allowed TYPO3 and PHP versions

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,6 +32,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
+			'php' => '5.3.0-0.0.0',
+			'typo3' => '6.0.0-6.2.99',
+			'templavoila' => '1.5.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
