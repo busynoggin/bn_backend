8a8e6ae4515e5e371109b524b88897ebd332d98f
Jeff Segars <jeff@busynoggin.com>
2015-02-28 22:07:36 -0600

    Adjust namespaced references

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,10 +2,8 @@
 
 if (TYPO3_MODE === 'BE') {
 	// Hook into group processing to include possible Static TSConfig
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauthgroup.php']['fetchGroups_postProcessing'][] = 'BusyNoggin\\BnBackend\\BackendLibrary->includeStaticTSConfigForGroups';
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauthgroup.php']['fetchGroups_postProcessing'][] = BusyNoggin\BnBackend\BackendLibrary::class . '->includeStaticTSConfigForGroups';
 
 	// Hide Allowed Exclude Fields within group record
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('TCEFORM.be_groups.non_exclude_fields.disabled = 1');
 }
-
-?>
