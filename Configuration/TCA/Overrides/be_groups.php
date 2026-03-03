1fe01900e9e2f7774a933f98a84c1e92997ff167
Claus Due <claus@namelesscoder.net>
2026-03-03 12:51:54 +0100

    Update TCA to current standards

--- Configuration/TCA/Overrides/be_groups.php
+++ Configuration/TCA/Overrides/be_groups.php
@@ -1,12 +1,11 @@
 <?php
-defined('TYPO3_MODE') or die();
-
 // Define the TCA for the static TS Config selector
 $staticTSConfigSelector = array(
 	'tx_bnbackend_tsconfig_files' => array(
 		'label' => 'LLL:EXT:bn_backend/locallang_db.xlf:be_groups.tx_bnbackend_tsconfig_files',
 		'config' => array(
 			'type' => 'select',
+            'renderType' => 'selectSingle',
 			'size' => 10,
 			'maxitems' => 100,
 			'items' => array(),
@@ -14,11 +13,6 @@ $staticTSConfigSelector = array(
 		)
 	),
 );
-
-
-if (version_compare(TYPO3_branch, '10.1', '<')) {
-    $staticTSConfigSelector['tx_bnbackend_tsconfig_files']['config']['enableMultiSelectFilterTextfield'] = true;
-}
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('be_groups', $staticTSConfigSelector);
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('be_groups', 'tx_bnbackend_tsconfig_files', '', 'before:TSconfig');
